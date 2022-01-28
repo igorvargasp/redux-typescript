d70a6e2d59def6373c7a348c93b224b128f55e80
igor <igorvargasp@hotmail.com>
2022-01-28 17:28:35 -0300

    react + typescript

---
+++ react+typescript/src/props/Child.tsx
@@ -0,0 +1,26 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+};
